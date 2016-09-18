03eba230251e06e44fb51da076f01df002d80456
Leonard Maximus Irenous Mensah <maxi@lmensah.com>
2016-09-18 19:41:42 +0000

    Player updated with title

--- pages/player/index.php
+++ pages/player/index.php
@@ -1,3 +1,15 @@
+<?php
+try{
+global $couchUrl;
+$couchUrl = 'http://pi:raspberry@127.0.0.1:5984';
+include "../../lib/couch.php";
+include "../../lib/couchClient.php";
+include "../../lib/couchDocument.php";
+
+}catch(Exception $err){
+	
+}
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -11,25 +23,45 @@
     </head>
     <body>
         <div class="container">
+        <div class="row"><div class="span12"><h2>
+        <?php
+		
+		global $couchUrl;
+		global $facilityId;
+		$resources = new couchClient($couchUrl, "ttel_resources");
+		$doc = $resources->getDoc($_GET['resid']);
+		$docAttachment = $doc->_attachments;
+		$arrayFiles = array();
+		foreach($docAttachment as $key => $value){
+				array_push($arrayFiles,$key);
+		}
+		$mystring = "http://".$_SERVER['SERVER_NAME'].":5984/ttel_resources/".$_GET['resid']."/".urlencode($arrayFiles[0])."";
+	try{
+		print($doc->title);
+	}catch(Exception $err){
+		print($err);
+	}
+			
+        ?>
+        </h2></div></div>
           <div class="row">
             <div class="span12">
                 <h2>&nbsp;</h2>
                     <div class="videoUiWrapper thumbnail">
                         <video width="800" height="400"  id="masterplayer">
                            <!-- <source src="http://ia700305.us.archive.org/18/items/CopyingIsNotTheft/CINT_Nik_H264_720.ogv" type="video/ogg"> -->
-                            <source src="<?php echo $_GET['url']; ?>" type="video/mp4">
-                            Your browser does not support the video tag. Download the video using the link below.
+                            <source src="<?php echo $mystring; ?>" type="video/mp4">
+                            Your browser does not support the video tag.
                         </video>
                     </div>
                     <h2>&nbsp;</h2>
               </div>
           </div>
-        You can <a href="<?php echo $_GET['url']; ?>" download>download the video here</a>.
         </div>
         <script type="text/javascript" src="assets/js/jquery-1.8.1.min.js"></script>
         <script type="text/javascript" src="assets/js/jquery.video-ui.js"></script>
         <script type="text/javascript" >
-            // $('#masterplayer').videoUI();
+            $('#masterplayer').videoUI();
         </script>
     </body>
 </html>
