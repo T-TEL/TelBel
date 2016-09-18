3f2d2564384e6064d72acf61f44d656cafe5b0e8
Bjoern <bjohas@users.noreply.github.com>
2016-09-18 18:22:05 +0000

    Updated video player

--- pages/player/index.php
+++ pages/player/index.php
@@ -18,17 +18,18 @@
                         <video width="800" height="400"  id="masterplayer">
                            <!-- <source src="http://ia700305.us.archive.org/18/items/CopyingIsNotTheft/CINT_Nik_H264_720.ogv" type="video/ogg"> -->
                             <source src="<?php echo $_GET['url']; ?>" type="video/mp4">
-                            Your browser does not support the video tag.
+                            Your browser does not support the video tag. Download the video using the link below.
                         </video>
                     </div>
                     <h2>&nbsp;</h2>
               </div>
           </div>
+        You can <a href="<?php echo $_GET['url']; ?>" download>download the video here</a>.
         </div>
         <script type="text/javascript" src="assets/js/jquery-1.8.1.min.js"></script>
         <script type="text/javascript" src="assets/js/jquery.video-ui.js"></script>
         <script type="text/javascript" >
-            $('#masterplayer').videoUI();
+            // $('#masterplayer').videoUI();
         </script>
     </body>
 </html>
